e12ef609e30ea115a77d25d8dd6cab89aef7b882
Emrah Sifoğlu <emrahsifoglu@gmail.com>
2023-08-31 16:36:53 +0200

    Create a HTTP server
    Add a dummy route
    Get query param and return a JSON response

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "net/http"
 )
 
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+    query := r.URL.Query()
+    name := query.Get("name")
+
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(fmt.Sprintf("Hello, %s", name)))
+}
+
 func main() {
-   fmt.Println("go-fetch-weather")
+    http.HandleFunc("/hello", handleRequest)
+    http.ListenAndServe(":9090", nil)
 }
